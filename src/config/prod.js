bace0e27686a2988db0a1c03f5c5bed9942efdf5
Michael <mhaviv18@gmail.com>
2018-02-27 20:14:00 -0500

    added keys to prod.js

--- src/config/prod.js
+++ src/config/prod.js
@@ -3,12 +3,12 @@ const firebase = require('firebase');
 
 // Initialize Firebase
 var config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyD3ddrEiszZyT-P7PDiI4_fYuRyUPc7qic",
+  authDomain: "portfolio-5ba9d.firebaseapp.com",
+  databaseURL: "https://portfolio-5ba9d.firebaseio.com",
+  projectId: "portfolio-5ba9d",
+  storageBucket: "portfolio-5ba9d.appspot.com",
+  messagingSenderId: "588445898377"
 };
 let fire = firebase.initializeApp(config);
 
