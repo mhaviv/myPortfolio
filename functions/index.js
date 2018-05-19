99422c2ee606b1b287a733facab152581b91c637
Michael <mhaviv18@gmail.com>
2018-05-19 17:14:15 -0400

    attempted to fix firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,30 @@
-/*************************Firebase Cloud Function************************************/
+/*
+**************************Firebase Cloud Function*************************************
+*/
 
 const functions = require('firebase-functions');
-const domain = 'sandbox534b67f6aba44c968328339f34014df4.mailgun.org'
-const apiKey = 'key-8051aa94de2ba7b626b7f0f1f8c876cd'
-const mailgun  = require('mailgun-js')({apiKey, domain})
+
+const domain = 'sandbox534b67f6aba44c968328339f34014df4.mailgun.org';
+const apiKey = 'key-8051aa94de2ba7b626b7f0f1f8c876cd';
+const mailgun = require('mailgun-js')({ apiKey, domain });
 
 exports.sendMailonWrite = functions.database.ref('/{Id}').onWrite((event) => {
-    var user = event.data.val()
-    console.log(user)
-    var data = {
-        from: 'submission@portfolioapp.io',
-        to: 'mhaviv18@gmail.com',
-        subject: 'This is a mail from '+ user.name,
-        text:`
-          Name: ${user.name}
-          Phone Number: ${user.number}
-          Email: ${user.email}
-          Message: ${user.message}
-        `
-      };
-      mailgun.messages().send(data, function (error, body) {
-        console.log(error);
-        console.log(body);
-      });
-      console.log("Mail sent dude ...")
+  const user = event.data.val();
+  console.log(user);
+  const data = {
+    from: 'submission@portfolioapp.io',
+    to: 'mhaviv18@gmail.com',
+    subject: `This is a mail from ${user.name}`,
+    text: `
+        Name: ${user.name}
+        Phone Number: ${user.number}
+        Email: ${user.email}
+        Message: ${user.message}
+      `,
+  };
+  mailgun.messages().send(data, (error, body) => {
+    console.log(error);
+    console.log(body);
+  });
+  console.log('Mail sent dude ...');
 });
